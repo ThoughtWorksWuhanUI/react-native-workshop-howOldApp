e5f6024fec94a5a125b6016ec049210123b15d65
mirrorhanyu <mirrorhanyu@gamil.com>
2018-05-30 16:07:56 +0800

    call microsoft face api to get info of selected image

--- app/components/howOld.js
+++ app/components/howOld.js
@@ -1,6 +1,8 @@
+import { Platform } from 'react-native';
 import React from 'react';
 import _ from 'lodash';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import { Alert, View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import RNFetchBlob from 'react-native-fetch-blob';
 
 const styles = StyleSheet.create({
   container: {
@@ -21,12 +23,37 @@ const styles = StyleSheet.create({
 });
 
 export default class HowOld extends React.Component {
+  componentDidMount() {
+    const imagePath = this.imagePath;
+    const imageUrl = Platform.OS === 'ios' ? _.replace(imagePath, 'file://', '')  : imagePath;
+    const headers = {
+      'Content-Type': 'application/octet-stream',
+      'Ocp-Apim-Subscription-Key': 'replace-this-with-your-secret-key',
+    };
+    RNFetchBlob.fetch('POST', 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceAttributes=age,gender', headers, RNFetchBlob.wrap(imageUrl))
+      .then((res) => {
+        const data = _.get(res, 'data');
+        const faceInfo = _.isEmpty(data) ? null : JSON.parse(data);
+        const error = _.get(faceInfo, 'error');
+
+        if(!_.isEmpty(error)) console.log("error", error);
+        if (_.isEmpty(faceInfo)) console.log("no people in the image");
+        console.log("result", res);
+      })
+      .catch((err) => {
+        Alert.alert(err);
+      });
+  }
+
+  get imagePath() {
+    return _.get(this.props, 'navigation.state.params.selectedImage')
+  }
+
   render() {
-    const imagePath = _.get(this.props, 'navigation.state.params.selectedImage');
     return (
       <View style={styles.container}>
         <View style={styles.imageContainer}>
-          <Image source={{uri: imagePath}} style={styles.image} resizeMode="contain"/>
+          <Image source={{uri: this.imagePath}} style={styles.image} resizeMode="contain"/>
         </View>
       </View>
     )
